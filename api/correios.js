3c97e434315dc470122948884f35ecb3f4ec6b6b
Giovani de Oliveira <giovanioliveira@outlook.com.br>
2022-05-17 10:13:53 -0300

    Improve logs

--- api/correios.js
+++ api/correios.js
@@ -7,17 +7,15 @@ module.exports = async (request, response) => {
     const { query } = request;
 
     if (!query.code) {
-      console.error('Invalid code');
-
-      response.status(422).json({ error: 'Invalid code' });
-    } else {
-      const object = await correios.track(query.code);
-
-      response.status(200).json(object);
+      return response.status(422).json({ error: 'Invalid code' });
     }
-  } catch (error) {
-    console.error(error.message);
 
-    response.status(500).json({ error: 'Error, try again later' });
+    const object = await correios.track(query.code);
+
+    return response.status(200).json(object);
+  } catch (error) {
+    console.error(error);
+
+    return response.status(500).json({ error: 'Error, try again later' });
   }
 };
